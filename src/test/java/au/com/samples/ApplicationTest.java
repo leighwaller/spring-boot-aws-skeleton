c323416568766b4947cae4fe2b33fcf243cad7e2
leigh_waller <leigh.waller2@icloud.com>
2018-11-27 21:35:08 +1100

    Fail test temporarily

--- src/test/java/au/com/samples/ApplicationTest.java
+++ src/test/java/au/com/samples/ApplicationTest.java
@@ -3,12 +3,16 @@ package au.com.samples;
 import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.Assert.assertFalse;
+
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
 public class ApplicationTest {
 
     @Test
     public void shouldStartApplication() {
         Application.main(new String[]{});
+
+        assertFalse(true);
     }
 
 }
