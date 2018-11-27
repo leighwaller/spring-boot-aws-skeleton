0040bd22165ed03c5d010a2009ed2a1fa0c9b756
leigh_waller <leigh.waller2@icloud.com>
2018-11-27 21:43:32 +1100

    Fixed test and add batch maven

--- src/test/java/au/com/samples/ApplicationTest.java
+++ src/test/java/au/com/samples/ApplicationTest.java
@@ -3,16 +3,12 @@ package au.com.samples;
 import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.Assert.assertFalse;
-
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
 public class ApplicationTest {
 
     @Test
     public void shouldStartApplication() {
         Application.main(new String[]{});
-
-        assertFalse(true);
     }
 
 }
